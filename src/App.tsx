c6eda864192dd0242b9a236673385510ca8ae40a
yellowbeee <709341218@qq.com>
2020-12-11 18:29:16 +0800

    feat: 接口定义，增加创建项目抽屉

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import EMPApp from '@emp-antd/base/App'
 import configStores from 'src/stores/config'
 import {routes} from 'src/configs/router'
 import CardList from 'src/components/CardList'
-import {Dropdown, Card, Avatar} from 'antd'
+import CreateProject from 'src/components/createProject'
+import {Dropdown, Card, Avatar, Button} from 'antd'
 import 'antd/dist/antd.css'
+import {StoreProvider} from './stores'
 const {Meta} = Card
 const PROJECTS = [
   {
@@ -25,52 +27,62 @@ const PROJECTS = [
     fullPath: '/Users/project/vue-base',
   },
 ]
-const App = () => (
-  <>
-    <EMPApp
-      stores={configStores}
-      routes={routes}
-      titleInfo={{
-        logo: 'https://setmefree.yy.com/Joyy.png',
-        text: (
-          <Dropdown
-            overlay={
-              <div style={{width: '400px'}}>
-                <CardList
-                  list={PROJECTS}
-                  nextPage={() => {}}
-                  span={24}
-                  page={1}
-                  pageSize={10}
-                  count={2}
-                  paginationOpt={{showSizeChanger: false}}
-                  // pagination={null}
-                  paginationJustify={'end'}
-                  cardDom={item => (
-                    <Card style={{margin: '0 0 10px'}}>
-                      <Meta
-                        avatar={<Avatar src={item.img} />}
-                        title={item.name}
-                        description={`地址：${item.fullPath}`}
-                      />
-                    </Card>
-                  )}
-                />
-              </div>
-            }
-            trigger={['click']}
-            placement="topRight">
-            {/* <Button color="#fff" type="link">
+const App = () => {
+  const [createProjectShow, createProjectShowAction] = useState(false)
+
+  return (
+    <StoreProvider>
+      <EMPApp
+        stores={configStores}
+        routes={routes}
+        titleInfo={{
+          logo: 'https://setmefree.yy.com/Joyy.png',
+          text: (
+            <Dropdown
+              overlay={
+                <div style={{width: '400px'}}>
+                  <CardList
+                    list={PROJECTS}
+                    nextPage={() => {}}
+                    span={24}
+                    page={1}
+                    pageSize={10}
+                    count={2}
+                    paginationOpt={{showSizeChanger: false}}
+                    // pagination={null}
+                    paginationJustify={'end'}
+                    cardDom={item => (
+                      <Card style={{margin: '0 0 10px'}}>
+                        <Meta
+                          avatar={<Avatar src={item.img} />}
+                          title={item.name}
+                          description={`地址：${item.fullPath}`}
+                        />
+                      </Card>
+                    )}
+                  />
+                  <Button type="primary" onClick={() => createProjectShowAction(true)}>
+                    创建项目
+                  </Button>
+                </div>
+              }
+              trigger={['click']}
+              placement="topRight">
+              {/* <Button color="#fff" type="link">
               react-base
             </Button> */}
-            <a style={{color: '#fff'}} href="javascript:;">
-              react-base
-            </a>
-          </Dropdown>
-        ),
-      }}
-      userProfile={null}
-    />
-  </>
-)
+              <a style={{color: '#fff'}} href="javascript:;">
+                react-base
+              </a>
+            </Dropdown>
+          ),
+        }}
+        userProfile={null}
+      />
+
+      {/*  */}
+      <CreateProject visible={createProjectShow} onClose={() => createProjectShowAction(false)} />
+    </StoreProvider>
+  )
+}
 export default App
