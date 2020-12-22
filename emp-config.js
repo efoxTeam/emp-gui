40db6e8dc11d83df99775644e18e6c797e3b3b23
nijina1 <nijina1@yy.com>
2020-12-22 14:59:57 +0800

    fix: 打包

--- emp-config.js
+++ emp-config.js
@@ -1,9 +1,22 @@
 const path = require('path')
 const withESbuild = require('@efox/emp-esbuild')
+
 module.exports = withESbuild(({config, env, empEnv}) => {
   console.log('empEnv===> 部署环境变量 serve模式不需要该变量', empEnv, env)
   const port = 8002
-  const publicPath = empEnv === 'dev' ? `http://localhost:${port}/` : './'
+  const isPro = env === 'production'
+  const publicPath = !isPro ? `http://localhost:${port}/` : './'
+
+  if (isPro) {
+    // config.optimization.usedExports(true)
+    // config.externals({
+    //   react: 'react',
+    //   antd: 'antd',
+    //   ['@ant-design/icons']: '@ant-design/icons',
+    // })
+    // config.devtool('cheap-module-source-map')
+  }
+
   config.resolve.alias.set('@', path.resolve('./', 'src'))
 
   config.output.publicPath(publicPath)
